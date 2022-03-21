2d64ef599ce76b677a75e92ed0a064cc563cf13e
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-03-21 13:44:01 -0300

    Alterado service_worker

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,2 +1,5 @@
-chrome.action.setBadgeText({text: '95'});
-chrome.action.setBadgeBackgroundColor({color: '#e82727'});
+chrome.storage.local.set({frequencia: '55'}); // precisa setar com heartRates do app.js
+chrome.storage.local.get(["frequencia"], ({ frequencia }) => {
+    chrome.action.setBadgeText({ text: frequencia });
+    chrome.action.setBadgeBackgroundColor({color: '#e82727'});
+});
