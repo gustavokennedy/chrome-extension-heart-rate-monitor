e387930d536ece36f85505d836101734cc9fb931
Gustavo Kennedy Renkel <gustavo@overall.cloud>
2022-03-21 09:12:07 -0300

    Alterado bug no Manifest

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,10 +1,2 @@
 chrome.action.setBadgeText({text: '95'});
 chrome.action.setBadgeBackgroundColor({color: '#e82727'});
-
-async function getCurrentTab() {/* ... */}
-let tab = await getCurrentTab();
-
-chrome.scripting.executeScript({
-  target: {tabId: tab.id},
-  files: ['content-script.js']
-});
